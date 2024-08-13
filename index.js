565bc772e8177ac62648e4cec037f479db7ebc31
ninadkadam123 <ninadkadam123@gmail.com>
2024-08-13 10:42:14 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ import userRouter from './routes/user.route.js';
 import { verifyToken } from './middleware/verifyToken.js';
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+const allowedOrigins = [process.env.CLIENT_URL];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
